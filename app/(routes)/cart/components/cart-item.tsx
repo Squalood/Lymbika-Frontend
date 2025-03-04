422a7442ab7b961ff06e5720a47e30beddab5f9f
Squalood <adriancas98@gmail.com>
2025-03-04 10:29:03 -0700

    oculto elemento no usado

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -3,7 +3,7 @@ import { formatPrice } from "@/lib/formatPrice";
 import { cn } from "@/lib/utils";
 import { ProductType } from "@/types/product";
 import { X } from "lucide-react";
-import Image from "next/image";
+//import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface CartItemProps {
