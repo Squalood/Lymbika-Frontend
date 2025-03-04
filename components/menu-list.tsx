f84cddedef3747ac10959a6d032e32df83bb869d
Squalood <adriancas98@gmail.com>
2025-03-04 10:01:08 -0700

    añadidos

--- components/menu-list.tsx
+++ components/menu-list.tsx
@@ -19,7 +19,7 @@ const MenuList = () => {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Pagina</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Sobre Nostros</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-3">
