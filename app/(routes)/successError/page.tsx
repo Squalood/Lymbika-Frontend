1e112a397b9161bc0f024a8ac218d3dd1a169b3a
Squalood <adriancas98@gmail.com>
2025-03-28 14:43:38 -0600

    add/successerror

---
+++ app/(routes)/successError/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const PageSuccessError = () => {
+
+
+    return ( 
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold">Hubo un error no se completo el pago 😔</h1>
+            <p className="mt-2">puedes intentarlo de nuevo.</p>
+        </div>
+     );
+}
+ 
+export default PageSuccessError;
