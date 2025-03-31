3df92472e44b17011ca92260ef812babf5b23bcb
Squalood <adriancas98@gmail.com>
2025-03-30 23:22:50 -0600

    add/userlogin-edit/navbar-edit/dashboard-1

--- app/(routes)/dashboard/account/page.tsx
+++ app/(routes)/dashboard/account/page.tsx
@@ -1,12 +1,12 @@
 // import { ProfileForm } from "@/components/forms/profile-form";
 // import { ProfileImageForm } from "@/components/forms/profile-image-form";
 
-import { getUserMeLoader } from "@/app/data/services/get-user-me-loader";
+//import { getUserMeLoader } from "@/app/data/services/get-user-me-loader";
 
 export default async function AccountRoute() {
-  const user = await getUserMeLoader();
-  const userData = user.data;
-  const userImage = userData?.image;
+  //const user = await getUserMeLoader();
+  //const userData = user.data;
+  //const userImage = userData?.image;
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 p-4">
