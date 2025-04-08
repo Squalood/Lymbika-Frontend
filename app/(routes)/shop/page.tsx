da7214529be163c8ab3379d18cfcbc986fd496b9
Squalood <adriancas98@gmail.com>
2025-04-08 11:46:15 -0600

    edit/shop

--- app/(routes)/shop/page.tsx
+++ app/(routes)/shop/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 import ChooseCategory from "@/components/choose-category";
 import FeaturedProducts from "@/components/featured-products";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Page() {
     return(  
         <div>
             <FeaturedProducts />
-            <ChooseCategory />
-            <div className="flex flex-wrap justify-evenly gap-2 p-4">
-                {["/promos/Promo-1.jpg", "/promos/Promo-2.jpg", "/promos/Promo-3.jpg"].map((src, i) => (
-                    <div key={i} className="w-[300px] overflow-hidden rounded-lg shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-xl hover:brightness-110">
-                    <Image 
-                        src={src}
-                        alt={`Promo ${i + 1}`}
-                        width={720}
-                        height={1280}
-                        className="w-full h-full object-cover"
-                    />
+            <div className="flex justify-center">
+                <div className="flex flex-col sm:flex-row justify-evenly gap-2 p-4">
+                    <div className="w-9/12 sm:w-1/2 flex justify-center mx-auto">
+                        <Image
+                            src="/promos/mediClub-promo.jpg"
+                            alt="membership image"
+                            width={690}
+                            height={690}
+                            className="max-w-full h-auto rounded-lg"
+                        />
                     </div>
-                ))}
+                    <div className="sm:w-1/2 text-center sm:text-left flex flex-col justify-center">
+                        <h2 className="text-2xl font-semibold mx-auto text-green-500">MediClub</h2>
+                        <Button className="mt-4 w-1/4 mx-auto">Mas Info</Button>
+                    </div>
+                </div>
             </div>
+            <ChooseCategory />
         </div>   
     );
 }
