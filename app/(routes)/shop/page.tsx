75fa631d05ad6992642c3174bace8f140e56bb3f
Squalood <adriancas98@gmail.com>
2025-04-08 11:16:37 -0600

    modify/shop-page

--- app/(routes)/shop/page.tsx
+++ app/(routes)/shop/page.tsx
@@ -1,13 +1,48 @@
 "use client";
 import ChooseCategory from "@/components/choose-category";
 import FeaturedProducts from "@/components/featured-products";
+import Image from "next/image";
+import { useState } from "react";
+import { X } from "lucide-react";
+
+const promoImages = [
+    "/promos/Promo-1.jpg",
+    "/promos/Promo-2.jpg",
+    "/promos/Promo-2.jpg",
+  ];
 
 export default function Page() {
+    
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+    const openModal = (src: string) => {
+        setSelectedImage(src);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalOpen(false);
+        setSelectedImage(null);
+    };
+
     return(  
         <div>
             <FeaturedProducts />
             <ChooseCategory />
-        </div>
-        
+            <div className="flex flex-wrap justify-evenly gap-2 p-4">
+                {["/promos/Promo-1.jpg", "/promos/Promo-2.jpg", "/promos/Promo-3.jpg"].map((src, i) => (
+                    <div key={i} className="w-[300px] overflow-hidden rounded-lg shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-xl hover:brightness-110">
+                    <Image 
+                        src={src}
+                        alt={`Promo ${i + 1}`}
+                        width={720}
+                        height={1280}
+                        className="w-full h-full object-cover"
+                    />
+                    </div>
+                ))}
+            </div>
+        </div>   
     );
 }
