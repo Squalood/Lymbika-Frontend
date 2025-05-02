c9e733b1c15d758bb31bd4637b29875653595880
Squalood <adriancas98@gmail.com>
2025-05-02 14:12:07 -0600

    update/programs

--- app/(routes)/programs/neurotherapy/components/neuroterapiaPage.tsx
+++ app/(routes)/programs/neurotherapy/components/neuroterapiaPage.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
 import es from "@/locals/es.json";
 import { MoveRight } from "lucide-react";
 
