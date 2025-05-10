058a529b5cbfb98b644aacc2ceab3dcc29e9ea2e
Squalood <adriancas98@gmail.com>
2025-05-10 00:00:47 -0600

    add/searchDoctor

--- app/(routes)/search/page.tsx
+++ app/(routes)/search/page.tsx
@@ -26,7 +26,7 @@ export default function SearchResultsPage() {
   return (
     <div className="max-w-6xl mx-auto px-4 py-10">
       <h1 className="text-2xl font-semibold mb-6">
-        Resultados para: "<span className="text-blue-600">{query}</span>"
+        Resultados para: &quot;<span className="text-blue-600">{query}</span>&quot;
       </h1>
 
       {/* Productos */}
