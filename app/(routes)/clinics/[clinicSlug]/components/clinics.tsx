fcf2023be97b35f1dde6133f8a7bfac591a4b86f
Squalood <adriancas98@gmail.com>
2025-08-04 11:25:36 -0600

    style: add clinics navs style

--- app/(routes)/clinics/[clinicSlug]/components/clinics.tsx
+++ app/(routes)/clinics/[clinicSlug]/components/clinics.tsx
@@ -26,16 +26,20 @@ export default function Clinics({ data }: ClinicsListProps) {
           <Link
             key={clinic.id}
             href={`/clinics/${clinic.slug}`}
-            className="group relative flex flex-col rounded-xl bg-cover bg-center text-white flex-[1_1_60%] sm:flex-[1_1_40%] sm:hover:flex-[2_1_58%] h-44 md:h-60 transition-all duration-500 ease-in-out"
+            className="group relative flex flex-col rounded-xl bg-cover bg-center text-white flex-[1_1_60%] sm:flex-[1_1_40%] sm:hover:flex-[2_1_58%] h-44 md:h-60 transition-all duration-500 ease-in-out overflow-hidden"
             style={{
               backgroundImage: `url(${clinic.heroImage?.url || "/placeholder.jpg"})`,
             }}
           >
-            <div className="flex-auto p-6 pb-0 text-white/90">
+            {/* Overlay con gradiente */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/40 to-transparent z-0 rounded-xl" />
+
+            {/* Contenido sobre la imagen */}
+            <div className="flex-auto p-6 pb-0 text-slate-200 relative z-10">
               <h3 className="text-base md:text-xl group-hover:text-white">{clinic.title}</h3>
               <span className="font-bold">{clinic.heroSubtitle}</span>
             </div>
-            <div className="p-4 pt-0">
+            <div className="p-4 pt-0 relative z-10">
               <div className="inline-flex items-center gap-2 text-xs md:text-sm font-medium group-hover:text-white/70">
                 Ver detalles
                 <ChevronRight size={20} />
@@ -53,11 +57,11 @@ export default function Clinics({ data }: ClinicsListProps) {
             href={`/clinics/${clinic.slug}`}
             className="group flex flex-col justify-end bg-muted rounded-xl h-40 p-4 text-gray-800 flex-[1_1_45%] sm:flex-[1_1_22%] transition-all"
             >
-            <div className="flex flex-col gap-1">
-                <Icon size={40} className="text-primary group-hover:scale-125 transition-transform" />
-                <h3 className="text-lg font-semibold group-hover:text-gray-600">{clinic.title}</h3>
-                <span className="text-muted-foreground text-sm">{clinic.heroSubtitle}</span>
-            </div>
+              <div className="flex flex-col gap-1">
+                  <Icon size={40} className="text-primary group-hover:scale-125 transition-transform" />
+                  <h3 className="text-lg font-semibold group-hover:text-gray-600">{clinic.title}</h3>
+                  <span className="text-muted-foreground text-sm line-clamp-2 max-w-52">{clinic.heroSubtitle}</span>
+              </div>
             </Link>
         );
         })}
