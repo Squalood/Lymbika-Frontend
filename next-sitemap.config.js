4bb012cb259409926cbf22f5c438f401a59df4aa
Squalood <adriancas98@gmail.com>
2025-08-07 11:29:31 -0600

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,41 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://lymbika.com',
-  generateRobotsTxt: true, // también genera robots.txt
+  generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
+  // Aquí defines las rutas manualmente si tienes rutas dinámicas
+  transform: async (config, path) => {
+    return {
+      loc: path, 
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
+  additionalPaths: async (config) => {
+    const dynamicRoutes = [
+      '/shop',
+      '/membership',
+      '/about',
+      '/shop',
+      '/category/medicamentos',
+      '/category/pediatrico',
+      '/category/cronicos',
+      '/category/cuidado-belleza',
+      '/category/especiales',
+      '/category/suplementos',
+      '/service/neurocirugia',
+      '/service/cirugia-general',
+      '/surgery/senos-paranasales'
+    ];
+
+    return dynamicRoutes.map((path) => ({
+      loc: `${config.siteUrl}${path}`,
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
