eac5e27270aac36396517034096e3cefbfbb041a
Squalood <adriancas98@gmail.com>
2026-01-06 01:39:58 -0700

    add review route

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,14 +1,16 @@
 import { SigninForm } from "@/components/forms/signin-form";
 
-export default function SingInRoute({
+export default async function SingInRoute({
   searchParams,
 }: {
-  searchParams: { callbackUrl?: string };
+  searchParams: Promise<{ callbackUrl?: string }>;
 }) {
+  const params = await searchParams;
+
   return(
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <SigninForm callbackUrl={searchParams.callbackUrl} />
+        <SigninForm callbackUrl={params.callbackUrl} />
       </div>
     </div>
   )
